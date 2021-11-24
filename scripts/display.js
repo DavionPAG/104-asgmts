c72d809dd0a53a694d75a1aa6754f898fb4882ce
Davion <daviongarcia@gmail.com>
2021-11-23 19:42:09 -0800

    cr 104

--- scripts/display.js
+++ scripts/display.js
@@ -2,14 +2,16 @@ function displayCards(hound) {
   let petsDiv = document.getElementById('hounds');
   let tmp =
     `
-  <div class="hound">
+  <div class="hound" id="${hound.id}">
   <h3>${hound.name}</h3>
   <p>Age: ${hound.age}</p>
   <p>Gender: ${hound.gender}</p>
   <p>Breed: ${hound.breed}</p>
+  <p>Height: ${hound.height}</p>
   <p>Service: ${hound.service}</p>
   <p>Owner: ${hound.owner}</p>
   <p>Tel: ${hound.phone}</p>
+  <button onclick="deletePet(${hound.id})" >Take</button>
   </div>
 
   `;
--- scripts/register.js
+++ scripts/register.js
@@ -16,7 +16,9 @@ let den = {
 
 let denList = document.getElementById('den')
 
-function Hound(name, age, gender, breed, service, owner, phone) {
+let id = 0;
+
+function Hound(name, age, gender, breed, service, owner, phone, height) {
   this.name = name;
   this.age = age;
   this.gender = gender;
@@ -24,13 +26,15 @@ function Hound(name, age, gender, breed, service, owner, phone) {
   this.service = service;
   this.owner = owner;
   this.phone = phone;
+  this.height = height;
+  this.id = id++
 }
 
-let balto = new Hound('Balto', 21, 'M', 'Tundra', 'Cleaning', 'Max', '206-789-6543')
+let balto = new Hound('Balto', 21, 'M', 'Tundra', 'Cleaning', 'Max', '206-789-6543', '3ft')
 
-let liger = new Hound('Liger', 5, 'F', 'Arctic', 'Day Care', 'TJ', '206-912-9876')
+let liger = new Hound('Liger', 5, 'F', 'Arctic', 'Day Care', 'TJ', '206-912-9876', '2ft')
 
-let lio = new Hound('Lio', 10, 'F', 'Steppe', 'Grooming', 'Sam', '206-327-5647')
+let lio = new Hound('Lio', 10, 'F', 'Steppe', 'Grooming', 'Sam', '206-327-5647', '4ft')
 
 den.hounds.push(balto, liger, lio);
 
@@ -41,10 +45,11 @@ let inputBreed = document.getElementById('breed');
 let inputService = document.getElementById('service');
 let inputOwner = document.getElementById('owner');
 let inputPhone = document.getElementById('phone');
+let inputHeight = document.getElementById('height');
+
 
 function register() {
-  console.log(inputPet.value, inputAge.value, inputGender.value, inputBreed.value, inputService.value, inputOwner.value, inputPhone.value);
-  let newHound = new Hound(inputPet.value, inputAge.value, inputGender.value, inputBreed.value, inputService.value, inputOwner.value, inputPhone.value)
+  let newHound = new Hound(inputPet.value, inputAge.value, inputGender.value, inputBreed.value, inputService.value, inputOwner.value, inputPhone.value, inputHeight.value)
   den.hounds.push(newHound)
   displayCards(newHound)
   clear();
@@ -55,11 +60,52 @@ displayCards(liger)
 displayCards(lio)
 
 function clear() {
-    inputPet.value = '';
-    inputAge.value = '';
-    inputGender.value = '';
-    inputBreed.value = '';
-    inputService.value = '';
-    inputOwner.value = '';
-    inputPhone.value = '';
+  inputPet.value = '';
+  inputAge.value = '';
+  inputGender.value = '';
+  inputBreed.value = '';
+  inputService.value = '';
+  inputOwner.value = '';
+  inputPhone.value = '';
+  inputHeight.value = '';
+}
+
+function deletePet(id) {
+  console.log('Byeeeeee! ' + id)
+  let remove = '';
+
+  den.hounds.forEach((hound, idx) => {
+    if (id === hound.id) {
+      remove = idx;
+    }
+  })
+
+  document.getElementById(id).remove()
+  den.hounds.splice(remove, 1)
+}
+
+function findHound() {
+  let input = document.getElementById('search').value;
+  console.log(input)
+
+  let found = ''
+
+  den.hounds.forEach((hound, idx) => {
+    if (input === hound.name) {
+      found = idx;
+    }
+    console.log('Found ' + found )
+  })
+  if(found !== '') {
+  alert(`
+  Name: ${den.hounds[found].name}\n
+  Age: ${den.hounds[found].age}\n
+  Gender: ${den.hounds[found].gender}\n
+  Breed: ${den.hounds[found].breed}\n
+  Height: ${den.hounds[found].height}\n
+  Service: ${den.hounds[found].service}\n
+  Owner: ${den.hounds[found].owner}\n
+  Tel: ${den.hounds[found].phone}
+  `)
+  } else alert('Not in this Pack!')
 }
